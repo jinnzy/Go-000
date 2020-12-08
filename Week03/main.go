e16be6f5519989b96f88669ce7adda687d0d50e3
jinzhy <839444083@qq.com>
2020-12-08 11:02:25 +0800

    第三周作业

--- Week03/main.go
+++ Week03/main.go
@@ -8,42 +8,39 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
-func main()  {
+func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	g, _ := errgroup.WithContext(ctx)
 	// 启动http1
-	http1 := http.Server{
-		Addr:              ":8111",
-	}
+	http1 := NewHttpServer(":8111")
 	g.Go(func() error {
-		if err := http1.ListenAndServe(); err != nil {
+		if err := http1.start(); err != nil {
 			cancel()
 			return err
 		}
 		return nil
 	})
 	// 启动http2
-	http2 := http.Server{
-		Addr:              ":8112",
-	}
+	http2 := NewHttpServer(":8112")
 	g.Go(func() error {
-		if err := http2.ListenAndServe(); err != nil {
+		if err := http2.start(); err != nil {
 			cancel()
 			return err
 		}
-		return nil	})
-
+		return nil
+	})
 
 	// 监听sig信号
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
 	go func() {
-		for  {
+		for {
 			select {
-			case s := <- c:
+			case s := <-c:
 				switch s {
 				case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
 					cancel()
@@ -56,18 +53,43 @@ func main()  {
 	// context取消后，关闭http server
 	go func() {
 		select {
-		case <- ctx.Done():
-			log.Println(ctx.Err())
-			http1.Shutdown(context.Background())
-			http1.Close()
-			http2.Shutdown(context.Background())
-			return
+		case <-ctx.Done():
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+			go func() {
+				if err := http1.shutdown(ctx); err != nil {
+					log.Println("http1 shutdown err: ", err)
+				}
+			}()
+			go func() {
+				if err := http2.shutdown(ctx); err != nil {
+					log.Println("http2 shutdown err: ", err)
+				}
+			}()
 		}
 
 	}()
-
 	if err := g.Wait(); err != nil {
-		log.Println(err)
-		log.Println("all exit")
+		log.Println("all exit: ", err)
 	}
+}
+
+type httpServer struct {
+	server http.Server
+}
+
+func NewHttpServer(addr string) *httpServer {
+	return &httpServer{
+		server: http.Server{
+			Addr: addr,
+		},
+	}
+}
+
+func (h *httpServer) start() error {
+	return h.server.ListenAndServe()
+}
+
+func (h *httpServer) shutdown(ctx context.Context) error {
+	return h.server.Shutdown(ctx)
 }
